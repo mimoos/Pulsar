075f04f34434e0b808cf8de9ab0a9c33f580943c
mimoos <mimoos@users.noreply.github.com>
2024-07-11 18:46:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,8 +31,8 @@ X_Features = X.columns
 
 #Model = SGDClassifier(loss="modified_huber")
 #Model = CalibratedClassifierCV(base_model)
-#Model = make_pipeline(StandardScaler(), LogisticRegression())
-Model = LogisticRegression()
+Model = make_pipeline(StandardScaler(), LogisticRegression())
+#Model = LogisticRegression()
 #Model = SVC(probability=True)
 Model.fit(X, y)
 
