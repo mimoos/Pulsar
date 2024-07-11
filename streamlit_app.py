6ab4b8418bfdeb75ea9d8d3b3d68880bf84eb04a
mimoos <mimoos@users.noreply.github.com>
2024-07-11 18:25:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,8 @@ X_Features = X.columns
 #Model = SGDClassifier()
 #Model = CalibratedClassifierCV(base_model)
 #Model = make_pipeline(StandardScaler(), LogisticRegression())
-#Model = LogisticRegression()
-Model = SVC(probability=True)
+Model = LogisticRegression()
+#Model = SVC(probability=True)
 Model.fit(X, y)
 
 Pulsar_data
