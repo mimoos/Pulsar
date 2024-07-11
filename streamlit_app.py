db2d84ac9ccc697a7b8bf254696a3efd2d90031a
mimoos <mimoos@users.noreply.github.com>
2024-07-11 18:49:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,9 +29,9 @@ X = Pulsar_data.drop(['Class'], axis = 1)
 
 X_Features = X.columns
 
-#Model = SGDClassifier(loss="modified_huber")
+Model = SGDClassifier(loss="modified_huber", penalty='l2', alpha=1e-3, n_iter=5, random_state=42)))
 #Model = CalibratedClassifierCV(base_model)
-Model = make_pipeline(StandardScaler(), LogisticRegression())
+#Model = make_pipeline(StandardScaler(), LogisticRegression())
 #Model = LogisticRegression()
 #Model = SVC(probability=True)
 Model.fit(X, y)
