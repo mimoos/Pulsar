6152a8f9ef43aff56f9c8905da03d25a069ccdae
mimoos <mimoos@users.noreply.github.com>
2024-07-11 17:01:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,6 @@ import plotly.graph_objects as go
 
 from sklearn.linear_model import SGDClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
 
@@ -32,7 +31,7 @@ X_Features = X.columns
 #Model = SGDClassifier()
 #Model = CalibratedClassifierCV(base_model)
 #Model = make_pipeline(StandardScaler(), LogisticRegression())
-Model = LinearRegression()
+Model = LogisticRegression()
 Model.fit(X, y)
 
 Pulsar_data
